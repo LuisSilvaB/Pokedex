769ed1cb6fb242237fa6424f26b544b091e3a22f
LuisSilvaB <LuisSilvaBalladares@hotmail.com>
2023-09-15 22:45:41 -0500

    Add screns, Tab Navigation, Edit tab navigation

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import { Image } from 'react-native'
+import FavoritesScreen from '../screens/Favorites';""
+import PokedexScreen from '../screens/Pokedex';
+import Account from '../screens/Account';
+
+const Tab = createBottomTabNavigator();  
+export default function Navigation() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name='Favorite' component={FavoritesScreen} options={{
+          tabBarLabel:"Favoritos", 
+          //Usando destructuración hacemos que el navigation controle color y size 
+          tabBarIcon: ({color,size}) => { 
+            return(
+              <Icon name='heart' size={size} color={color}/>
+            )
+          }
+        }}/>
+      <Tab.Screen name='Pokedex' component={PokedexScreen} options={{
+          tabBarLabel:"",
+          tabBarIcon: () => RenderPokeball(), 
+      }}/>
+      <Tab.Screen name='Account' component={Account} options={{
+          tabBarLabel:"Account",
+          //Usando destructuración hacemos que el navigation controle color y size 
+          tabBarIcon:({color,size})=>{
+            return(
+              <Icon name='user' color={color} size={size}/>
+            )
+          }
+      }}/>
+    </Tab.Navigator>
+  )
+}
+
+// Creación de función para que me devuelva una imagen 
+function RenderPokeball(){
+  return(
+    <Image source={require('../assets/Pokeball.png')} style={{
+      width:70,
+      height:70,
+      top:-15,
+    }}/>
+  )
+}
---
+++ src/screens/Favorites.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import React from 'react'
+
+export default function FavoritesScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Favorites</Text>
+    </SafeAreaView>
+  )
+}
